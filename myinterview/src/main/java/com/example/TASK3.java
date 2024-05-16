c649d44713eba6520ce2f284ffa64aa83acfc030
Bruno Silverio <bcamilo94@gmail.com>
2024-05-15 22:43:49 -0300

    feat: add TASK3

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -1,7 +1,10 @@
 package com.example;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
 
 /**
  * Write a list and add an aleatory number of Strings. In the end, print out how
@@ -9,6 +12,37 @@ import java.util.Iterator;
  *
  */
 public class TASK3 {
+	public static void main(String[] args) {
 
-  
+		Scanner scanner = new Scanner(System.in);
+		List<String> stringList = new ArrayList<>();
+		
+		boolean loop = true;
+
+		while (loop) {
+						
+			System.out.println("Write random strings (Press 0 to stop): "); // Pressione ENTER para digitar a proxima string
+			String item = scanner.nextLine();
+
+			while (!item.equalsIgnoreCase("0")) { // Adiciona cada item a lista
+				stringList.add(item);
+				item = scanner.nextLine();
+			}
+			
+			Set<String> distinctItems = new HashSet<>(stringList); // Com a funcao Set pegamos a quantidade de itens distintos da lista
+
+			System.out.println("There exist " + distinctItems.size() + " distinct items on the list.");
+
+			System.out.println("Try again? (Y/N)");
+			String op = scanner.nextLine();
+			
+			if (op.equalsIgnoreCase("N")) {
+				loop = false;
+			}
+			
+			distinctItems.clear();
+			stringList.clear();	
+		}
+		scanner.close();
+	}
 }
